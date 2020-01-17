4595ee884bf13167bde1364831a1a1763c872eee
Unknown <wolfla@hotmail.fr>
2020-01-17 16:12:14 +0100

    feat : modify index (is authenticated) + just move my templates in the appropriate folder...

--- WatchBlog/videos/views.py
+++ WatchBlog/videos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
+def index(request):
+    template = loader.get_template('videos/index.html')
+    # user = authenticate(username='Lou', password='admin')
+    context = {'user': request.user}
+    print(request.user.is_authenticated)
+    print(request.user)
+    return HttpResponse(template.render(context, request))
