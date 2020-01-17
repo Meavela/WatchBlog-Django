701b5cdc14603d238bac30cdaaae568a694669a8
Unknown <wolfla@hotmail.fr>
2020-01-17 15:38:34 +0100

    feat : add model video

--- WatchBlog/videos/models.py
+++ WatchBlog/videos/models.py
@@ -2,3 +2,19 @@ from django.db import models
 from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    rate = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+
+    TYPES_VIDEOS = [
+        ('Film', 'Film'),
+        ('Serie', 'Serie');
+    ]
+    types_videos = models.CharField(
+        max_length = 10,
+        choices = TYPES_VIDEOS,
+        default = 'Film'
+    )
